e6b23bc6c1219ef277c3394b4d6bdcbf4c194d57
Mar Lla <44751468+marllaest2075@users.noreply.github.com>
2020-07-27 17:54:05 -0700

    Clase de autorizacion lista

---
+++ Src/Intento001/Shared/AuthenticationConfig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intento001.Shared
+{
+    public static class AuthenticationConfig
+    {
+        public const string key = "CB1E08171A72D65C8AC9CD758531FD55B104F81918D0A20C2EFFC3EE533003DC";
+        public static string CreateJsonWebToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim("userName","Admin")
+            };
+            var token = new JwtSecurityToken(
+                "Http://localhost:52875",
+                "Http://localhost:52875",
+                claims,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
